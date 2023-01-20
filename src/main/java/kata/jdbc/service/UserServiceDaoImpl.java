b85c236ebe202edc6afd96fcef69ab2478f636d5
Evgeny Voinov <joekobarnd@gmail.com>
2023-01-20 15:28:22 +0300

    7

--- src/main/java/kata/jdbc/service/UserServiceDaoImpl.java
+++ src/main/java/kata/jdbc/service/UserServiceDaoImpl.java
@@ -1,41 +1,46 @@
 package kata.jdbc.service;
 
-import kata.jdbc.dao.UserDaoJDBCImpl;
+import kata.jdbc.dao.UserDao;
 import kata.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceDaoImpl implements UserService {
 
-    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    //    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    private final UserDao userDao;
+
+    public UserServiceDaoImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     @Override
     public void createUsersTable() {
-        userDaoJDBC.createUsersTable();
+        userDao.createUsersTable();
     }
 
     @Override
     public void dropUsersTable() {
-        userDaoJDBC.dropUsersTable();
+        userDao.dropUsersTable();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        userDaoJDBC.saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
     }
 
     @Override
     public void removeUserById(long id) {
-        userDaoJDBC.removeUserById(id);
+        userDao.removeUserById(id);
     }
 
     @Override
     public List<User> getAllUsers() {
-        return userDaoJDBC.getAllUsers();
+        return userDao.getAllUsers();
     }
 
     @Override
     public void cleanUsersTable() {
-        userDaoJDBC.cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 }
